2aed5f37cb2b11b72bf7f41d80f703bca7b5bf07
Jörg Müller <nexyon@gmail.com>
2025-11-12 22:53:30 +0100

    Thumb-Key v2: single layer version.

--- layout_evaluation/src/metrics/bigram_metrics/thumb_key.rs
+++ layout_evaluation/src/metrics/bigram_metrics/thumb_key.rs
@@ -11,7 +11,7 @@ use keyboard_layout::{
 
 use serde::Deserialize;
 
-use std::ops::{Add, Sub};
+use std::ops::{Add, Sub, Mul, Div, Rem};
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 struct Vec2(i8, i8);
@@ -24,11 +24,43 @@ impl Add for Vec2 {
     }
 }
 
+impl Add<i8> for Vec2 {
+    type Output = Self;
+
+    fn add(self, other: i8) -> Self {
+        Vec2(self.0 + other, self.1 + other)
+    }
+}
+
 impl Sub for Vec2 {
     type Output = Self;
 
     fn sub(self, other: Self) -> Self {
         Vec2(self.0 - other.0, self.1 - other.1)
+    }
+}
+
+impl Mul<i8> for Vec2 {
+    type Output = Self;
+
+    fn mul(self, other: i8) -> Self {
+        Vec2(self.0 * other, self.1 * other)
+    }
+}
+
+impl Div<i8> for Vec2 {
+    type Output = Self;
+
+    fn div(self, other: i8) -> Self {
+        Vec2(self.0 / other, self.1 / other)
+    }
+}
+
+impl Rem<i8> for Vec2 {
+    type Output = Self;
+
+    fn rem(self, other: i8) -> Self {
+        Vec2(self.0 % other, self.1 % other)
     }
 }
 
@@ -58,21 +90,16 @@ impl ThumbKey {
     }
 }
 
-fn layer_to_movement(layer: u8) -> Vec2
+fn key_to_position(k: &LayerKey) -> Vec2
 {
-    // swipe-directions: no-swipe, up-left, up, up-right, left, right, down-left, down, down-right
-    match layer
-    {
-        1 => Vec2(-1, -1),
-        2 => Vec2(0, -1),
-        3 => Vec2(1, -1),
-        4 => Vec2(-1, 0),
-        5 => Vec2(1, 0),
-        6 => Vec2(-1, 1),
-        7 => Vec2(0, 1),
-        8 => Vec2(1, 1),
-        _ => Vec2(0, 0),
-    }
+    Vec2::from(k.key.matrix_position) / 3
+}
+
+fn key_to_movement(k: &LayerKey) -> Vec2
+{
+    let position = Vec2::from(k.key.matrix_position) / 3;
+    let center = position * 3 + 1;
+    Vec2::from(k.key.matrix_position) - center
 }
 
 impl BigramMetric for ThumbKey {
@@ -89,11 +116,11 @@ impl BigramMetric for ThumbKey {
         _total_weight: f64,
         _layout: &Layout,
     ) -> Option<f64> {
-        let k1_swipe_direction = layer_to_movement(k1.layer);
-        let k2_swipe_direction = layer_to_movement(k2.layer);
+        let k1_swipe_direction = key_to_movement(k1);
+        let k2_swipe_direction = key_to_movement(k2);
 
-        let k1_start_pos = Vec2::from(k1.key.matrix_position);
-        let k2_start_pos = Vec2::from(k2.key.matrix_position);
+        let k1_start_pos = key_to_position(k1);
+        let k2_start_pos = key_to_position(k2);
 
         let k1_end_pos = k1_start_pos + k1_swipe_direction;
         let k2_end_pos = k2_start_pos + k2_swipe_direction;
